a9280097a51c2da3c78886cc1f84c86939d6f620
seonkyo <tjs0466@naver.com>
2024-11-06 18:55:54 +0900

    chore: husky 설정

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'test', 'chore', 'misc']],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'never', ['sentence-case', 'pascal-case']],
+  },
+};
